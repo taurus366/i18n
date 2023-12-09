f229836b0b4bbb241b1cc1367805bf67c9fec0f2
Arcerr <taurus..ali47>
2023-12-10 00:33:07 +0200

    .

--- src/main/java/org/system/i18n/CustomI18NProvider.java
+++ src/main/java/org/system/i18n/CustomI18NProvider.java
@@ -1,14 +1,21 @@
 package org.system.i18n;
 
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.i18n.I18NProvider;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.system.i18n.model.dto.LanguageDTO;
+import org.system.i18n.model.dto.MultiLangualTextAreaDTO;
 import org.system.i18n.service.LanguageService;
-
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.button.Button;
+import java.awt.*;
 import java.text.MessageFormat;
 import java.util.*;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class CustomI18NProvider implements I18NProvider {
@@ -88,5 +95,76 @@ public class CustomI18NProvider implements I18NProvider {
 
     public LanguageDTO getLanguageByLocale(String locale) {
         return this.languageService.getLanguageByLocale(locale);
+    }
+
+    public MultiLangualTextAreaDTO multiLanguageTextArea(Map<Long, String> data) {
+        CustomI18NProvider provider = new CustomI18NProvider(languageService);
+        final List<LanguageDTO> allActiveLanguages = provider.getAllActiveLanguages();
+        AtomicInteger currentLanguageIndex = new AtomicInteger();
+
+
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.getStyle()
+                .set("display", "flex")
+                .set("flex-direction", "column")
+                .set("align-items", "center")
+                .set("width", "100%");
+
+        TextArea textArea = new TextArea();
+        textArea.getStyle()
+                .set("width", "100%")
+                .set("max-height", "58px")
+                .set("min-height", "58px");
+
+        Section sectionTextAreaAndFlag = new Section();
+        sectionTextAreaAndFlag.getStyle()
+                .set("display", "flex")
+                .set("width", "100%")
+                .set("align-items", "center")
+                .set("justify-content", "space-around");
+
+
+        CountryFlagT flag = new CountryFlagT(allActiveLanguages.get(currentLanguageIndex.get()).getCode(), false);
+
+        flag.addAttachListener(event -> {
+            Long selectedLanguageId = allActiveLanguages.get(currentLanguageIndex.get()).getId();
+            String dataText = data.getOrDefault(selectedLanguageId, ""); // Get the text associated with the selected language ID
+            textArea.setValue(dataText);
+        });
+
+        flag.addClickListener(event -> {
+
+            if(currentLanguageIndex.get() + 1 < allActiveLanguages.size()){
+                flag.setCountry(allActiveLanguages.get(currentLanguageIndex.addAndGet(1)).getCode());
+            } else {
+                currentLanguageIndex.set(0);
+                flag.setCountry(allActiveLanguages.get(currentLanguageIndex.get()).getCode());
+            }
+
+            Long selectedLanguageId = allActiveLanguages.get(currentLanguageIndex.get()).getId();
+            String dataText = data.getOrDefault(selectedLanguageId, ""); // Get the text associated with the selected language ID
+
+            textArea.setValue(dataText);
+        });
+        textArea.addValueChangeListener(event -> {
+            Long selectedLanguageId = allActiveLanguages.get(currentLanguageIndex.get()).getId();
+               data.put(selectedLanguageId, textArea.getValue());
+
+        });
+
+        Button btnSave = new Button("Save");
+
+
+        sectionTextAreaAndFlag.add(textArea, flag);
+
+        layout.add(sectionTextAreaAndFlag, btnSave);
+
+        MultiLangualTextAreaDTO dto = new MultiLangualTextAreaDTO();
+        dto.setTextArea(textArea);
+        dto.setData(data);
+        dto.setButton(btnSave);
+        dto.setHorizontalLayout(layout);
+
+        return dto;
     }
 }
